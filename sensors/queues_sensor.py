12ee6a2491c66ccddd3b52fbd986fe151c578804
Ian Price <iprice@salesforce.com>
2022-09-20 14:16:57 -0700

    add logic to enable marking a queue as 'quorum' for creation or for existing consumption in sensor code

--- sensors/queues_sensor.py
+++ sensors/queues_sensor.py
@@ -8,10 +8,7 @@ import eventlet
 
 from st2reactor.sensor.base import Sensor
 
-DESERIALIZATION_FUNCTIONS = {
-    'json': json.loads,
-    'pickle': pickle.loads
-}
+DESERIALIZATION_FUNCTIONS = {"json": json.loads, "pickle": pickle.loads}
 
 
 class RabbitMQQueueSensor(Sensor):
@@ -23,30 +20,44 @@ class RabbitMQQueueSensor(Sensor):
     It is capable of simultaneously consuming from multiple queues. Each message is
     dispatched to stackstorm as a `rabbitmq.new_message` TriggerInstance.
     """
+
     def __init__(self, sensor_service, config=None):
-        super(RabbitMQQueueSensor, self).__init__(sensor_service=sensor_service, config=config)
+        super(RabbitMQQueueSensor, self).__init__(
+            sensor_service=sensor_service, config=config
+        )
 
         self._logger = self._sensor_service.get_logger(name=self.__class__.__name__)
-        self.host = self._config['sensor_config']['host']
-        self.username = self._config['sensor_config']['username']
-        self.password = self._config['sensor_config']['password']
+        self.host = self._config["sensor_config"]["host"]
+        self.username = self._config["sensor_config"]["username"]
+        self.password = self._config["sensor_config"]["password"]
 
-        queue_sensor_config = self._config['sensor_config']['rabbitmq_queue_sensor']
-        self.queues = queue_sensor_config['queues']
+        queue_sensor_config = self._config["sensor_config"]["rabbitmq_queue_sensor"]
+        self.queues = queue_sensor_config["queues"]
         if not isinstance(self.queues, list):
             self.queues = [self.queues]
-        self.deserialization_method = queue_sensor_config['deserialization_method']
+
+        self.quorum_queues = queue_sensor_config.get("quorum_queues", list())
+        self.deserialization_method = queue_sensor_config.get(
+            "deserialization_method", "json"
+        )
 
         supported_methods = DESERIALIZATION_FUNCTIONS.keys()
-        if self.deserialization_method and self.deserialization_method not in supported_methods:
-            raise ValueError('Invalid deserialization method specified: %s' %
-                             (self.deserialization_method))
+        if (
+            self.deserialization_method
+            and self.deserialization_method not in supported_methods
+        ):
+            raise ValueError(
+                "Invalid deserialization method specified: %s"
+                % (self.deserialization_method)
+            )
 
         self.conn = None
         self.channel = None
 
     def run(self):
-        self._logger.info('Starting to consume messages from RabbitMQ for %s', self.queues)
+        self._logger.info(
+            "Starting to consume messages from RabbitMQ for %s", self.queues
+        )
         # run in an eventlet in-order to yield correctly
         gt = eventlet.spawn(self.channel.start_consuming)
         # wait else the sensor will quit
@@ -58,8 +69,12 @@ class RabbitMQQueueSensor(Sensor):
 
     def setup(self):
         if self.username and self.password:
-            credentials = PlainCredentials(username=self.username, password=self.password)
-            connection_params = pika.ConnectionParameters(host=self.host, credentials=credentials)
+            credentials = PlainCredentials(
+                username=self.username, password=self.password
+            )
+            connection_params = pika.ConnectionParameters(
+                host=self.host, credentials=credentials
+            )
         else:
             connection_params = pika.ConnectionParameters(host=self.host)
 
@@ -67,9 +82,16 @@ class RabbitMQQueueSensor(Sensor):
         self.channel = self.conn.channel()
         self.channel.basic_qos(prefetch_count=1)
 
+        quorum_args = {"x-queue-type": "quorum"}
+
         # Setup Qs for listening
         for queue in self.queues:
-            self.channel.queue_declare(queue=queue, durable=True)
+            if queue not in self.quorum_queues:
+                self.channel.queue_declare(queue=queue, durable=True)
+            else:
+                self.channel.queue_declare(
+                    queue=queue, durable=True, arguments=quorum_args
+                )
 
             def callback(ch, method, properties, body, queue_copy=queue):
                 self._dispatch_trigger(ch, method, properties, body, queue_copy)
@@ -78,11 +100,13 @@ class RabbitMQQueueSensor(Sensor):
 
     def _dispatch_trigger(self, ch, method, properties, body, queue):
         body = self._deserialize_body(body=body)
-        self._logger.debug('Received message for queue %s with body %s', queue, body)
-        body = body.decode('utf-8')
+        self._logger.debug("Received message for queue %s with body %s", queue, body)
+        body = body.decode("utf-8")
         payload = {"queue": queue, "body": body}
         try:
-            self._sensor_service.dispatch(trigger="rabbitmq.new_message", payload=payload)
+            self._sensor_service.dispatch(
+                trigger="rabbitmq.new_message", payload=payload
+            )
         finally:
             self.channel.basic_ack(delivery_tag=method.delivery_tag)
 
